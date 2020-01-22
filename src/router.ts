a33270a372b000468044c912e3e6f5e91376ee3a
Avik Nandi <aviknandi@yahoo.com>
2020-01-22 11:46:32 +0530

    fixed router

--- src/router.ts
+++ src/router.ts
@@ -19,24 +19,6 @@ export default new Router({
       path: '/:companyId',
       name: 'home',
       component: Statistics,
-    },
-    // {
-    //   path: '/about',
-    //   name: 'about',
-      // route level code-splitting
-      // this generates a separate chunk (about.[hash].js) for this route
-      // which is lazy-loaded when the route is visited.
-      // component: () => import(/* webpackChunkName: "about" */ './views/About.vue'),
-    // },
-    // {
-    //   path: '/show-data',
-    //   name: 'show-data',
-    //   component: ShowData
-    // },
-    // {
-    //   path: '/data-view/:name',
-    //   name: 'data-view',
-    //   component: Home
-    // }
+    }
   ],
 });
